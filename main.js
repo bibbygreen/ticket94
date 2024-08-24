53a1b1916ffcbf934b249d5737e3f09ecf14b608
bibbygreen <a20034425@gmail.com>
2024-08-25 04:28:33 +0800

    modify dbConfig

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // 載入模組
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 require("dotenv").config();
 require("./utils/seatCleaner");
 
